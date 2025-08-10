430a1df4c484f008f4a9dfdf7d122a39860997d4
Amit Kumar <useramit2016@gmail.com>
2025-08-10 20:03:58 +0530

    Title: Profile to Dashboard

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -46,7 +46,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
+          title: 'Dashboard',
           tabBarIcon: ({ color }) => (
             <IconSymbol size={28} name="person.fill" color={color} />
           ),
